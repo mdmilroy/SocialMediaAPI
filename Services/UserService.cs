cd8a2aa972249c4200baefd6b75ad10cece2b248
Michael Milroy <mdmilroy@gmail.com>
2020-04-26 15:04:24 -0400

    added UserService

---
+++ Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserService
+    {
+        private readonly Guid _userId;
+
+        public UserService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateUser(SignUp model)
+        {
+            var entity =
+                new User()
+                {
+                    UserId = _userId,
+                    Name = model.Name,
+                    Email = model.Email,
+                    Password = model.Password
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Users.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
