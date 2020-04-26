a5f25ce7d1d1397904db7411b8cee9c4b389c2eb
blake.e.kendall@gmail.com <blake.e.kendall@gmail.com>
2020-04-26 16:18:44 -0400

    re-added Post Models

---
+++ Models/PostAPost.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Posts
+{
+    public class PostAPost
+    {
+        
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+
+        [MaxLength(8000)]
+        public string Text { get; set; }
+
+        [Required]
+        [Display(Name = "Created By")]
+        public User Author { get; set; }
+    }
+}
