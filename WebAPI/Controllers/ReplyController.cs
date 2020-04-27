a59abb2a5a08c6013a73557d6264832b33de1e26
blake.e.kendall@gmail.com <blake.e.kendall@gmail.com>
2020-04-27 15:41:30 -0400

    fixed a spacing issue in ReplyController

--- WebAPI/Controllers/ReplyController.cs
+++ WebAPI/Controllers/ReplyController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Models;
+using Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,49 @@ using System.Web.Http;
 
 namespace WebAPI.Controllers
 {
+    [Authorize]
+    [RoutePrefix("api/replies")]
     public class ReplyController : ApiController
     {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            ReplyService replyService = CreateReplyService();
+            var replies = replyService.GetCommentReplies();
+            return Ok(replies);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(PostReplyOnComment reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.ReplyOnComment(reply))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateReplyService();
+
+            if (!service.DeleteReply(id))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+
+        private ReplyService CreateReplyService()
+        {
+            var authorId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(authorId);
+            return replyService;
+        }
     }
 }
