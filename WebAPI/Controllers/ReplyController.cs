ec20c9e0df6a83495fda95151a224579d80a877f
Michael Milroy <mdmilroy@gmail.com>
2020-04-27 19:46:53 -0400

    added reply service and controller

---
+++ WebAPI/Controllers/ReplyController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity;
+using Models;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/replies")]
+    public class ReplyController : ApiController
+    {
+
+        [HttpPost] 
+        public IHttpActionResult Post(PostReplyToComment reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.ReplyOnComment(reply))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(EditAReply replyToEdit)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.UpdateReply(replyToEdit))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateReplyService();
+
+            if (!service.DeleteReply(id))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+
+        private ReplyService CreateReplyService()
+        {
+            var authorId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(authorId);
+            return replyService;
+        }
+    }
+}
