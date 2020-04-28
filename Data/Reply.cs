b4ca3765482a769491dfb07c2ac23239bc7ccfe6
Michael Milroy <mdmilroy@gmail.com>
2020-04-28 18:34:43 -0400

    replyService and controller; updated Db

--- Data/Reply.cs
+++ Data/Reply.cs
@@ -4,10 +4,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Data
 {
-    public class Reply : Comment
+    public class Reply/* : Comment*/
     {        
         [Key]
         public int ReplyId { get; set; }
+
+        public string ReplyText { get; set; }
+        public string Author { get; set; }
+        public Guid UserId { get; set; }
+
+
+        [ForeignKey("RepliedComment")]
+        public int CommentId { get; set; }
+        public virtual Comment RepliedComment { get; set; }
         //public Comment ReplyComment { get; set; }
     }
 }
