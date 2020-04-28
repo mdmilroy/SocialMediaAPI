092c65120f79db47b5a8ea5d154790017dc030c8
Michael Milroy <mdmilroy@gmail.com>
2020-04-28 19:11:18 -0400

    updated name

--- WebAPI/Controllers/ReplyController.cs
+++ WebAPI/Controllers/ReplyController.cs
@@ -22,6 +22,14 @@ namespace WebAPI.Controllers
             return replyService;
         }
 
+        [HttpGet]
+        public IHttpActionResult GetCommentReplies(int id)
+        {
+            ReplyService replyService = CreateReplyService();
+            var replies = replyService.GetReplies(id);
+            return Ok(replies);
+        }
+
         [HttpPost]
         public IHttpActionResult Post(PostReply replyToPost)
         {
